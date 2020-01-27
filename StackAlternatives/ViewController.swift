051e7c4ff75ce614c976e4a479164bbb5f06a2b8
rotem <nevgauker@gmail.com>
2020-01-27 18:34:59 +0200

    Initial Commit

---
+++ StackAlternatives/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackAlternatives
+//
+//  Created by Rotem Nevgauker on 27/01/2020.
+//  Copyright © 2020 Rotem Nevgauker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
